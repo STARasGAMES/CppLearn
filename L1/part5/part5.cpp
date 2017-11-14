6220b0faed95e2cda14b3d0ca7d7b35f6a3fab61
Some Sheat <doterpoter@outlook.com>
2017-11-14 16:42:35 +0200

    part5 ex 1

---
+++ L1/part5/part5.cpp
@@ -0,0 +1,37 @@
+// part5.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+char someChar = '3';
+char arrayOfChar[] = "someshet";
+char* pointerToChar = &someChar;
+char* pointerToArrayOfChar = &arrayOfChar[1];
+char* pointerToPointerToChar = pointerToChar;
+int arrayOfInt[10];
+int *pointerToArrayOfInt = arrayOfInt;
+
+const int constantInt = 1;
+const int constantInt2 = 2;
+ int constantInt3 = 3;
+const int* pointerToConstantInt = &constantInt;
+const int* const pointerToIntConstant = &constantInt;
+
+int main()
+{
+	std::cout
+		<< constantInt << "\n"
+		<< *pointerToConstantInt << "\n"
+		<< *pointerToIntConstant << "\n"
+		;
+	pointerToConstantInt = &constantInt2;
+	std::cout
+		<< constantInt << "\n"
+		<< *pointerToConstantInt << "\n"
+		<< *pointerToIntConstant << "\n"
+		;
+	system("Pause");
+    return 0;
+}
+
