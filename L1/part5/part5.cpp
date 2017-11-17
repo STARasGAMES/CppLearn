d2995245b401a84cc3d69434d9aa753c0e354204
Some Sheat <doterpoter@outlook.com>
2017-11-17 22:18:46 +0200

    part5 ex13

--- L1/part5/part5.cpp
+++ L1/part5/part5.cpp
@@ -8,48 +8,27 @@
 #include <vector>
 #include <algorithm>
 
-int countOfStr(std::string s, std::string s2) {
-	int res = 0;
-	for (size_t i = 0; i < s.length(); i++)
-	{
-		bool find = true;
-		for (size_t j = 0, const n2 = s2.length(); j < n2; j++)
-		{
-			if (s[i + j] != s2[j]) {
-				find = false;
-				break;
-			}
-		}
-		res += find;
-	}
+struct Date {
+	int day, month, year;
+};
+
+Date inputDate() 
+{
+	Date res;
+	std::cout << "Input day month and year\n";
+	std::cin >> res.day >> res.month >> res.year;
 	return res;
 }
 
-int countOfStrC(char *s, char *s2) {
-	int res = 0;
-	int i = 0;
-	for (char *p1 = s; *p1 != 0; p1++) {
-		bool find = true;
-		for (size_t i = 0; *(s2+i)!=0; i++)
-		{
-			if (*(p1+i) != *(s2 + i))
-			{
-				find = false;
-				break;
-			}
-		}
-		std::cout << "\n";
-		res += find;
-	}
-	std::cout << "\n";
-	return res;
+void outputDate(Date& date)
+{
+	std::cout << date.day << "." << date.month << "." << date.year << "\n";
 }
 
 int main()
 {
-	char s1[] = "xabaacbaxabb";
-	char s2[] = "ab";
-	std::cout << countOfStrC(s1, s2);
+	Date date = inputDate();
+	outputDate(date);
 	system("Pause");
 	return 0;
 }
